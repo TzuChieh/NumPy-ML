a6b5f2ec6665886e87ab431a09f54dc64a37d9a0
Tzu-Chieh Chang <r05922176@ntu.edu.tw>
2024-04-08 21:06:27 +0800

    add cost function abstraction

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import random
+from abc import ABC, abstractmethod
 
 import numpy as np
 
@@ -9,10 +10,38 @@ def sigmoid(z):
 def derived_sigmoid(z):
     return sigmoid(z) * (1.0 - sigmoid(z))
 
+class CostFunction(ABC):
+    @abstractmethod
+    def func(self, a, y):
+        pass
+
+    @abstractmethod
+    def delta(self, a, y, z):
+        pass
+
+class QuadraticCost(CostFunction):
+    def func(self, a, y):
+        """
+        Basically computing the MSE between activations `a` and desired output `y`. The 0.5 multiplier is
+        to make its derived form cleaner (for convenience).
+        """
+        return 0.5 * np.linalg.norm(a - y) ** 2
+    
+    def delta(self, a, y, z):
+        """
+        Note that the `(a - y)` term is @f$ \\partial C_x / \\partial a_output @f$.
+        """
+        return (a - y) * derived_sigmoid(z)
+    
+# class CrossEntropyCost(CostFunction):
+#     def func(self, a, y):
+
+
 class Network:
-    def __init__(self, layer_sizes):
+    def __init__(self, layer_sizes, cost_type=QuadraticCost):
         self.num_layers = len(layer_sizes)
         self.layer_sizes = layer_sizes
+        self.cost = cost_type()
         self.init_scaled_gaussian_weights()
 
     def feedforward(self, a):
@@ -43,21 +72,20 @@ class Network:
                 self._update_mini_batch(mini_batch_data, eta)
             
             if test_data:
-                num_right_answers = self.evaluate_performance(test_data)
-                num_test_data = len(test_data)
-                performance_info = (
-                    f"performance: {num_right_answers} / {num_test_data} ({num_right_answers / num_test_data})")
+                performance_info = f"performance: {self.performance_report(test_data)}"
             else:
                 performance_info = "performance evaluation skipped"
 
-            print(f"epoch {ei} / {num_epochs}; {performance_info}")
+            print(f"epoch {ei + 1} / {num_epochs}; {performance_info}")
 
-    def evaluate_performance(self, test_data):
+    def performance_report(self, test_data):
         """
-        @return Number of correct outputs.
+        @return A string that contains information of the network performance.
         """
         results = [(np.argmax(self.feedforward(x)), np.argmax(y)) for x, y in test_data]
-        return sum(1 for network_y, y in results if network_y == y)
+        num_right_answers = sum(1 for network_y, y in results if network_y == y)
+        num_test_data = len(test_data)
+        return f"{num_right_answers} / {num_test_data} ({num_right_answers / num_test_data})"
 
     def init_gaussian_weights(self):
         """
@@ -106,7 +134,7 @@ class Network:
             activations.append(sigmoid(z))
 
         # Backward pass
-        delta = self._derived_cost(activations[-1], y) * derived_sigmoid(zs[-1])
+        delta = self.cost.delta(activations[-1], y, zs[-1])
         del_b[-1] = delta
         del_w[-1] = np.matmul(delta, activations[-2].transpose())
         for layer_idx in reversed(range(0, self.num_layers - 2)):
@@ -117,9 +145,3 @@ class Network:
             del_w[layer_idx] = np.matmul(delta, activations[layer_idx].transpose())
 
         return del_b, del_w
-        
-    def _derived_cost(self, output_activations, y):
-        """
-        @return @f$ \\partial C_x / \\partial a_output @f$
-        """
-        return output_activations - y
