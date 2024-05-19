[bugfix] max pooling wrongly assign gradients to higher dimensions

common/vector.py
@@ -78,7 +78,12 @@ def argmax_lower(a: np_type.NDArray, num_dims) -> np_type.NDArray:
     @param num_dims Number of lower dimensions to perform argmax in. `a.shape[0]` is the highest dimension and
     `a.shape[-1]` is the lowest dimension.
     @return Tuple of array of indices into the last `num_dims` dimensions of `a`, where each index corresponds to the
-    maximum element found. The tuple is suitable for use with NumPy advanced indexing.
+    maximum element found. The tuple is suitable for use with NumPy advanced indexing to retrieve the actual values:
+    ```python
+    >>> a = np.array([[1, 2, 3], [4, 5, 6]])
+    >>> a[[0, 1], *argmax_lower(a, 1)]
+    [3, 6]
+    ```
     """
     higher_shape = a.shape[:-num_dims]
     lower_shape = a.shape[-num_dims:]
@@ -130,7 +135,7 @@ def pool_shape(matrix_shape, kernel_shape, stride_shape) -> np_type.NDArray:
 
 def sliding_window_view(matrix: np_type.NDArray, window_shape, stride_shape=(1,), is_writeable=False) -> np_type.NDArray:
     """
-    @param is_writeable Whether the returned view is writeable or not. For safety, the view is read-only. See
+    @param is_writeable Whether the returned view is writeable or not. For safety, the view is read-only by default. See
     `numpy.lib.stride_tricks.as_strided()` for more details. Basically, you at least need to ensure that the
     write-to locations are not overlapping in vectorized operations.
     """

model/layer.py
@@ -363,13 +363,14 @@ class FullyConnected(Layer):
     
     def update_params(self, bias, weight):
         assert self.is_trainable
-        assert self._bias.dtype == com.REAL_DTYPE, f"{self._bias.dtype}"
-        assert self._weight.dtype == com.REAL_DTYPE, f"{self._weight.dtype}"
         assert np.array_equal(self._bias.shape, bias.shape), f"shapes: {self._bias.shape}, {bias.shape}"
         assert np.array_equal(self._weight.shape, weight.shape), f"shapes: {self._weight.shape}, {weight.shape}"
 
         self._bias = bias
         self._weight = weight
+
+        assert self._bias.dtype == com.REAL_DTYPE, f"{self._bias.dtype}"
+        assert self._weight.dtype == com.REAL_DTYPE, f"{self._weight.dtype}"
 
     def weighted_input(self, x, cache):
         z = self.try_get_from_cache(cache, 'z')
@@ -492,6 +493,9 @@ class Convolution(Layer):
         self._bias = bias
         self._weight = weight
 
+        assert self._bias.dtype == com.REAL_DTYPE, f"{self._bias.dtype}"
+        assert self._weight.dtype == com.REAL_DTYPE, f"{self._weight.dtype}"
+
     def weighted_input(self, x, cache):
         z = self.try_get_from_cache(cache, 'z')
         if z is not None:
@@ -541,7 +545,6 @@ class Convolution(Layer):
         return (del_b, del_w)
 
     def backpropagate(self, x, delta, cache):
-        assert delta.dtype == com.REAL_DTYPE, f"{delta.dtype}"
         assert self._with_compatible_output_shape(delta)
 
         # Prepare kernel and delta
@@ -648,7 +651,6 @@ class Pool(Layer):
         return (np.array([]), np.array([]))
 
     def backpropagate(self, x, delta, cache):
-        assert delta.dtype == com.REAL_DTYPE, f"{delta.dtype}"
         assert self._with_compatible_input_shape(x)
         assert self._with_compatible_output_shape(delta)
 
@@ -676,7 +678,7 @@ class Pool(Layer):
                 # has 0 weight)
                 case com.EPooling.MAX:
                     pool_argmax = vec.argmax_lower(x_pool, len(self._kernel_shape))
-                    dCdx[*higher_slices, *pool_argmax] += delta[pool_idx]
+                    dCdx_pool[..., *pool_argmax] += delta[pool_idx]
                 # Similar to the case of max pooling, average pooling is equivalent to an imaginary weight of
                 # the reciprocal of number of pool elements
                 case com.EPooling.AVERAGE: